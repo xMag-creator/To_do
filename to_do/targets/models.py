8000f35d9682cede413e54d1e1c93abb4ce03b26
jerry <p.kobylarz@outlook.com>
2023-04-08 17:15:16 +0200

    'targets' app models added

--- to_do/targets/models.py
+++ to_do/targets/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Items(models.Model):
+    """
+    Class representing the structure of tasks to be performed.
+
+    
+    Fields: 
+        name (CharField): Item name.
+        finished (BooleanField): Information about completing the item.
+        parent_item (ForeignKey): Parent item pk.
+    """
+    name = models.CharField(max_length=256)
+    finished = models.BooleanField(default=False)
+    parent_item = models.ForeignKey('self',
+                                    on_delete=models.CASCADE,
+                                    null=True,
+                                    blank=True,
+                                    related_name='children')
